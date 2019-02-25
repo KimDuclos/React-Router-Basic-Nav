c194ba0307d821c69756debbcfa7cf52cf3b88e5
Kim Duclos <kimduclos@gmail.com>
2019-02-25 13:45:50 -0700

    done through 'steps for 'declaring' your routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'; 
+import { Route} from 'react-router-dom';
 import App from './App';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 ReactDOM.render(
     <Router>
-      <App />
+      <div>
+        <App />
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+      </div>
     </Router>,
-    document.getElementById('root')
-  );
+    document.getElementById('root'));
 
 
